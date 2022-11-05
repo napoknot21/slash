f217f295e7dab9a231a4f71b1c3297c2818f80f2
garnierk <Kevin_Garnier@outlook.fr>
2022-11-05 01:57:09 +0100

    #14 Token's test improvement

--- test/test_token.c
+++ test/test_token.c
@@ -28,20 +28,20 @@ static int test_new(char *str, enum type type, enum type_spec spec)
 int test_token()
 {
     int bool = 1;
-    bool &= test_cmd("cd", CMD, INTERNAL); // Valid for EXTERNAL
-    bool &= test_new("<", REDIRECT, STDIN);
-    bool &= test_new(">", REDIRECT, STDOUT);
-    bool &= test_new(">|", REDIRECT, STDOUT_TRUNC);
-    bool &= test_new(">>", REDIRECT, STDOUT_APPEND);
-    bool &= test_new("2>", REDIRECT, ERROUT);
-    bool &= test_new("2>|", REDIRECT, ERROUT_TRUNC);
-    bool &= test_new("2>>", REDIRECT, ERROUT_APPEND);
-    bool &= test_new("|", REDIRECT, PIPE);
-    bool &= test_new("I'm an argument", ARG, SPEC_NONE);
-    bool &= test_new("I'm", ARG, SPEC_NONE);
-    bool &= test_new("I'", ARG, SPEC_NONE);
-    bool &= test_new("<2", ARG, SPEC_NONE);
-    bool &= test_new("2>g", ARG, SPEC_NONE);
-    bool &= test_new(">2>", ARG, SPEC_NONE);
+    bool &= ASSERT(test_cmd("cd", CMD, INTERNAL)); // Valid for EXTERNAL
+    bool &= ASSERT(test_new("<", REDIRECT, STDIN));
+    bool &= ASSERT(test_new(">", REDIRECT, STDOUT));
+    bool &= ASSERT(test_new(">|", REDIRECT, STDOUT_TRUNC));
+    bool &= ASSERT(test_new(">>", REDIRECT, STDOUT_APPEND));
+    bool &= ASSERT(test_new("2>", REDIRECT, ERROUT));
+    bool &= ASSERT(test_new("2>|", REDIRECT, ERROUT_TRUNC));
+    bool &= ASSERT(test_new("2>>", REDIRECT, ERROUT_APPEND));
+    bool &= ASSERT(test_new("|", REDIRECT, PIPE));
+    bool &= ASSERT(test_new("I'm an argument", ARG, SPEC_NONE));
+    bool &= ASSERT(test_new("I'm", ARG, SPEC_NONE));
+    bool &= ASSERT(test_new("I'", ARG, SPEC_NONE));
+    bool &= ASSERT(test_new("<2", ARG, SPEC_NONE));
+    bool &= ASSERT(test_new("2>g", ARG, SPEC_NONE));
+    bool &= ASSERT(test_new(">2>", ARG, SPEC_NONE));
     return bool;
 }
