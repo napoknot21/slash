6be0ba8cb732b19d326ac006d09608c3193dcfa8
napoknot21 <kinbrian5@hotmail.com>
2022-11-23 10:20:58 +0100

    Changement de char par struct string pour pwd.c

--- src/pwd.c
+++ src/pwd.c
@@ -12,58 +12,78 @@
 #include "path.h"
 #include "slasherrno.h"
 
+#define RET_LINE "\n"
+
 int builtin_pwd (int std, int err, int argc, char **argv)
 {
-	char *path = getenv("PWD");
+	struct string *path = make_string(getenv("PWD"));
 
-	//int kind = LOGICAL_PATH;
+	struct string *retLine = make_string(RET_LINE);
+
+	if (path == NULL) {
+		write(err, "pwd: Internal issue with PWD variable env\n",43);
+		return STATUS_PWD_ERROR;
+	}
 
 	switch (argc) {
-
+		
 		case 1 :
-			write (std, path, strlen(path));
-			write(std,"\n",1);
+			append(path, retLine);
+			write (std, c_str(path), strlen(c_str(path)));
+			free_string(retLine);
+			free_string(path);
 			return STATUS_PWD_SUCCESS;
 
 		case 2 :
 			if (!strcmp(argv[1], "-L")) {
-				write(std, path, strlen(path));
-				write(std,"\n",1);
+				append(path, retLine);
+				write(std, c_str(path), strlen(c_str(path)));
+				free_string(retLine);
+				free_string(path);
 				return STATUS_PWD_SUCCESS;
-
+			
 			} else if (!strcmp(argv[1], "-P")) {
 				//kind = PHYSICAL_PATH;
 
 			} else {
-				write(err, "pwd: Not valid option!\n", 24);
+				write(err, "pwd: Not valid option!\n", 23);
+				free_string(retLine);
+				free_string(path);
 				return STATUS_PWD_ERROR;
 			}
 			break;
 
 		default :
-			write (err, "pwd: Expected 0 or 1 argument!\n", 32);
+			write (err, "pwd: Expected 0 or 1 argument!\n", 31);
+			free_string(retLine);
+			free_string(path);
 			return STATUS_PWD_ERROR;
-
-	}
+			
+	}		
 
 	/**
 	 * Symlinks case
 	 */
-
+	
 	char buff[PHYSICAL_PATH_BUFFER];
-
-	//struct string *path_str = make_string(path);
-
-	char *sympath = realpath(path, buff);
+	
+	char *sympath = realpath(c_str(path), buff);
 
 	if (sympath == NULL) {
-		write (err, "Internal problem with the symbolic path...\n", 44);
-
+		write (err, "pwd: Internal issue with the symbolic path...\n", 47);
+		free_string(path);
 		return STATUS_PWD_ERROR;
 	}
 
-	write (std, sympath, strlen(sympath));
-	write(std,"\n",1);
+	struct string *pathsym = make_string(sympath);
+	
+	append (pathsym, retLine);
+
+	write (std, c_str(pathsym), strlen(c_str(pathsym)));
+
+	free_string(retLine);
+	free_string(path);
+	free_string(pathsym);
 
 	return STATUS_PWD_SUCCESS;
 }
